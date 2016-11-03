56449794e29fb91d389f94db483c3a2a4bdd4fd0
mndowne <mndowne@gmail.com>
2016-11-03 16:26:48 -0500

    video 34 about 4:00 in to it

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var todoControler = require('./controler/todocontroler');
+var app = express();
+var exphbs = require('express-handlebars');
+
+
+//set up template engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//static files
+app.use(express.static('./public'));
+
+
+//fire controllers
+todoControler(app);
+
+
+//listen to port
+
+app.listen(3000, function(){ console.log('server is up on port 3000');});
+
